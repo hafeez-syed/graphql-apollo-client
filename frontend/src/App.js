dcbea168ca509bb7491f6fa69a6b374935a29d97
Hafeez Syed <feeziman007@hotmail.com>
2018-12-10 12:58:30 +1100

    graphql apollo-client setup

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        recipes {
+          id
+          title
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+class App extends Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+export default App;
